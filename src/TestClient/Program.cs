5db534e2e148b075dc0733a450c318347fedea14
Matija Koželj <matija.kozelj@tompit.com>
2025-11-27 09:34:37 +0100

    feat: update project references from package references

--- src/TestClient/Program.cs
+++ src/TestClient/Program.cs
@@ -6,9 +6,6 @@ internal static class Program
 {
 	public static async Task Main(string[] args)
 	{
-		Application.RegisterMicroService("Connected.ServiceModel.Storage.FileSystem.dll");
-		Application.RegisterMicroService("Connected.Core.Authorization.Default.dll");
-
-		await Application.StartDefaultApplication(args);
+		await Application.Start(args);
 	}
 }
